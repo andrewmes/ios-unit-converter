83da9a0a028820ff91e7265c59fdc068ccc808c7
Andrew Messer <51286614+andrewmes@users.noreply.github.com>
2022-02-13 22:24:30 -0700

    first commit

---
+++ Project_2_AM/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Project_2_AM
+//
+//  Created by Andrew Messer on 2/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var milesInput: UITextField!
+    
+    @IBOutlet weak var kilosInput: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func doConvertBtn(_ sender: UIButton) {
+        let inputMiles1 = milesInput.text
+        let inputKilos1 = kilosInput.text
+        if (inputMiles1 == "" && inputKilos1 == "")
+        {
+            milesInput.text = ""
+            kilosInput.text = ""
+        }
+        var inputMiles = Float(inputMiles1!)
+        if (inputMiles1 == "" || inputMiles1 == nil)
+        {
+            inputMiles = 0.0
+            milesInput.text = String(0.0)
+        }
+        else
+        {
+            let mileToKilo = (inputMiles! / 0.62137119)
+            kilosInput.text = String(mileToKilo)
+        }
+    }
+    
+    @IBAction func doConvertKmBtn(_ sender: Any) {
+        let inputMiles1 = milesInput.text
+        let inputKilos1 = kilosInput.text
+        if (inputKilos1 == "" && inputMiles1 == "")
+        {
+            milesInput.text = ""
+            kilosInput.text = ""
+        }
+        var inputKilos = Float(inputKilos1!)
+        if (inputKilos1 == "" || inputKilos1 == nil)
+        {
+            inputKilos = 0.0
+            kilosInput.text = String(0.0)
+        }
+        else
+        {
+            let kiloToMile = (inputKilos! * 0.62137119)
+            milesInput.text = String(kiloToMile)
+        }
+    }
+}
+
